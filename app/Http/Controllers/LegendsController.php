0eae1cd0f3cfb7a47ab2ded15a43b11dad1aa095
Renaud <renaudfradin@gmail.com>
2024-02-01 16:59:22 +0100

    init model , install pest , fillament

---
+++ app/Http/Controllers/LegendsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLegendsRequest;
+use App\Http\Requests\UpdateLegendsRequest;
+use App\Models\Legends;
+
+class LegendsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLegendsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Legends $legends)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Legends $legends)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLegendsRequest $request, Legends $legends)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Legends $legends)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_02_01_150741_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('slug', 255);
+            $table->string('quote', 255);
+            $table->string('quote_author', 255);
+            $table->longText('content');
+            $table->string('image')->nullable();
+            $table->boolean('active');
+            $table->unsignedInteger('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('posts');
+    }
+};
